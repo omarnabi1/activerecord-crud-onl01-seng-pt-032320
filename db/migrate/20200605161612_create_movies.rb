1c52dbcdea139cd02c393b36484ba7cb63e2975e
Omar Nabi <onabi20@gmail.com>
2020-06-05 20:57:49 +0000

    Done.

---
+++ db/migrate/20200605161612_create_movies.rb
@@ -0,0 +1,13 @@
+Class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |movie|
+      movie.string :title
+      movie.integer :release_date
+      movie.string :director
+      movie.string :lead
+      movie.boolean :in_theaters
+
+      movie.timestamps null: false
+    end
+  end
+end
